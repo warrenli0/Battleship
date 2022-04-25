b163e214bf2406c919c285df57be4a1c6fd9181a
David Yates <djwyates@gmail.com>
2022-04-25 00:24:07 -0500

    Fixed overflow error when parsing extremely large user inputs for ship position

--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,6 @@
 use crate::ship::{ShipPosition};
 
-const MAX_NUM_ROWS: usize = 26;
+const MAX_NUM_ROWS: usize = 26;  // max number of rows is 26 because we use an alphanumeric positioning system (with only 1 letter)
 pub struct Settings {
     game_impl: GameImpl,
     num_rows: usize,
@@ -25,29 +25,35 @@ impl Settings {
         self.num_cols
     }
 
-    pub fn parse_alphanum_pos(&self, pos: &str) -> Result<(usize, usize), ()> {
+    pub fn parse_alphanum_pos(&self, pos: &str) -> Result<(usize, usize), &str> {
         let chars: Vec<char> = pos.chars().collect();
-        if chars.len() < 2 { return Err(()); }
+        if chars.len() < 2 { return Err("Missing digit or letter. Can only parse the pattern: <digit><letter>"); }
 
         let first_char: char = *chars.get(0).unwrap();
         let last_char: char = *chars.get(chars.len() - 1).unwrap();
         if !first_char.is_digit(10) || !last_char.is_ascii_alphabetic() {
-            return Err(());
+            return Err("Missing digit or letter. Can only parse the pattern: <digit><letter>");
         }
 
-        let mut row: usize = first_char.to_digit(10).unwrap() as usize;  // make sure this doesn't panic if first_char is too large for a usize
+        let mut row: usize = first_char.to_digit(10).unwrap() as usize;
         for idx in 1..chars.len() - 1 {
             let next_char: char = *chars.get(idx).unwrap();
             if !next_char.is_digit(10) {
-                return Err(());
+                return Err("Too many letters. Can only parse the pattern: <digit><letter>");
             }
-            row *= 10;
-            row += next_char.to_digit(10).unwrap() as usize;
+            let next_digit = next_char.to_digit(10).unwrap() as usize;
+            row = match row.checked_mul(10) {
+                Some(row) => match row.checked_add(next_digit) {
+                    Some(row) => row,
+                    None => return Err("Row number is too large for a usize")
+                },
+                None => return Err("Row number is too large for a usize")
+            };
         }
 
         let col: usize = last_char.to_ascii_uppercase() as usize;
         if row == 0 || row - 1 >= self.num_rows || col < 65 || col - 65 >= self.num_cols {
-            return Err(());
+            return Err("Position is outside of the board");
         }
 
         Ok((row - 1, col - 65))
